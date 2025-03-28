472fe248f630d63e27a1329399fc9d2b4479720d
Ian Fernandes <fernandesi2244@gmail.com>
2025-03-27 23:39:36 -0400

    log on every node

--- run_glue_2a.py
+++ run_glue_2a.py
@@ -500,7 +500,7 @@ def main():
     # Setup logging
     logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                         datefmt = '%m/%d/%Y %H:%M:%S',
-                        level = logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+                        level = logging.INFO)
     logger.warning("Process rank: %s, device: %s, distributed training: %s, 16-bits training: %s",
                     args.local_rank, args.device, bool(args.local_rank != -1), args.fp16)
 
